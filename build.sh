6e6fa3d790478b5f788202f670776695e401ad65
IntPtr <lxy.lixiaoyan@gmail.com>
2018-01-15 17:24:54 +0800

    update

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 inkscape ./logo.svg -d 384 -e ./logo.png
 inkscape ./logo-text.svg -d 384 -e ./logo-text.png
 inkscape ./logo-text-dense.svg -d 384 -e ./logo-text-dense.png
-inkscape ./logo-icon.svg -d 384 -e ./logo-icon.png
+inkscape ./icon.svg -d 384 -e ./icon.png
+inkscape ./cover.svg -d 384 -e ./cover.png
